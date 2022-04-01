e577b5630f72e89eab1053697ca8ad5f63a19af0
gjango <38881781+gjango@users.noreply.github.com>
2022-04-01 13:37:32 +0400

    commit unicorn

---
+++ unicorn.js
@@ -0,0 +1,31 @@
+import { ... } from 🦄
+
+const TestFunction = () => {
+  const { userInfo } = useContext(AuthContext) // getting user object from auth context
+    const wrapperRef = useRef(null) // init ref
+    
+      const handleClickOutside = event => {
+        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+          ...
+        }
+      } // handle click outside the component
+      
+        useEffect(() => {
+          document.addEventListener('mousedown', handleClickOutside)
+          return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+          }
+       }) // capture click
+
+    
+    return (
+      <Wrapper ref={wrapperRef}> // passing ref
+      ...
+      <Wrapper />
+}
+
+const Wrapper = styled.div`
+  ...
+`
+
+export default Unicorn
